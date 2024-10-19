d090d059a611a9b067fbf4946bb404830f76f11c
Daniick56 <danielballesteros22@outlook.com>
2024-10-19 09:23:13 -0500

    saving changes

--- src/scriptbd/usuario_bd.sql
+++ src/scriptbd/usuario_bd.sql
@@ -2,14 +2,15 @@ CREATE DATABASE IF NOT EXISTS `usuario_bd`;
 USE `usuario_bd`;
 
 CREATE TABLE IF NOT EXISTS `usuario` (
-`documento` varchar(11) NOT NULL,  
-`nombre` varchar(80) NOT NULL,
-  `profesion` varchar(200) NOT NULL,
-  `edad` int(3) NOT NULL,
-  `direccion` varchar(200) NOT NULL,
-  `telefono` varchar(10) NOT NULL,
-  `tipo` int(2) NOT NULL
+  `documento` VARCHAR(11) NOT NULL,
+  `nombre` VARCHAR(80) NOT NULL,
+  `profesion` VARCHAR(200) NOT NULL,
+  `edad` INT(3) NOT NULL,
+  `direccion` VARCHAR(200) NOT NULL,
+  `telefono` VARCHAR(10) NOT NULL,
+  `tipo` INT(2) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`documento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-INSERT INTO usuario (documento,nombre,profesion,edad,direccion,telefono,tipo) VALUES ("admin","Administrador","",0,"","",1)
+INSERT INTO usuario (documento,nombre,profesion,edad,direccion,telefono,tipo, password) VALUES ("1094044519","Administrador","",0,"","",1, 1234)
